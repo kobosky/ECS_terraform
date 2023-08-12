4ddd1c29a381e75e1f497e9114e6641781ceb5d0
kobosky <kobosky_3@yahoo.com>
2023-08-12 02:54:15 +0100

    destroy

--- project/delete_image.sh
+++ project/delete_image.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
+
+# # Set your AWS region
+# REGION="eu-west-2"
+
+# REPO_NAME="my-first-ecr-repo"
+
+# # List ECR repositories
+# echo "Listing ECR repositories..."
+# REPOS=$(aws ecr describe-repositories --region $REGION --output text --query 'repositories[*].repositoryName')
+
+# # Print the list of repositories
+# echo "Available ECR repositories:"
+# echo "$REPOS"
+
+# # Iterate through repositories and list/delete images
+# for REPO_NAME in $REPOS; do
+#     # List images in the current repository
+#     echo "Listing images in $REPO_NAME..."
+#     IMAGES=$(aws ecr list-images --region $REGION --repository-name $REPO_NAME --output text --query 'imageIds[*].imageTag')
+
+#     # Print the list of images
+#     echo "Images in $REPO_NAME:"
+#     echo "$IMAGES"
+
+#     # Loop through images and delete them
+#     for IMAGE_TAG in $IMAGES; do
+#         echo "Deleting image $REPO_NAME:$IMAGE_TAG..."
+#         aws ecr batch-delete-image --region $REGION --repository-name $REPO_NAME --image-ids imageTag=$IMAGE_TAG
+#     done
+# done
+
+#!/bin/bash
+
 # Set your AWS region
 REGION="eu-west-2"
 
-REPO_NAME="my-first-ecr-repo"
+# Set the repository name to focus on
+TARGET_REPO_NAME="my-first-ecr-repo"
 
-# List ECR repositories
-echo "Listing ECR repositories..."
-REPOS=$(aws ecr describe-repositories --region $REGION --output text --query 'repositories[*].repositoryName')
+# List images in the targeted repository
+echo "Listing images in $TARGET_REPO_NAME..."
+IMAGES=$(aws ecr list-images --region $REGION --repository-name $TARGET_REPO_NAME --output text --query 'imageIds[*].imageTag')
 
-# Print the list of repositories
-echo "Available ECR repositories:"
-echo "$REPOS"
+# Print the list of images
+echo "Images in $TARGET_REPO_NAME:"
+echo "$IMAGES"
 
-# Iterate through repositories and list/delete images
-for REPO_NAME in $REPOS; do
-    # List images in the current repository
-    echo "Listing images in $REPO_NAME..."
-    IMAGES=$(aws ecr list-images --region $REGION --repository-name $REPO_NAME --output text --query 'imageIds[*].imageTag')
-
-    # Print the list of images
-    echo "Images in $REPO_NAME:"
-    echo "$IMAGES"
-
-    # Loop through images and delete them
-    for IMAGE_TAG in $IMAGES; do
-        echo "Deleting image $REPO_NAME:$IMAGE_TAG..."
-        aws ecr batch-delete-image --region $REGION --repository-name $REPO_NAME --image-ids imageTag=$IMAGE_TAG
-    done
+# Loop through images and delete them
+for IMAGE_TAG in $IMAGES; do
+    echo "Deleting image $TARGET_REPO_NAME:$IMAGE_TAG..."
+    aws ecr batch-delete-image --region $REGION --repository-name $TARGET_REPO_NAME --image-ids imageTag=$IMAGE_TAG
 done
